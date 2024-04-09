621d9431a3e1592860ea1a2b42d4ca2366a5e002
GTM16 <146287094+GTM16@users.noreply.github.com>
2024-04-08 22:19:58 -0300

    exerc4

---
+++ BackEnd/backend-2024-master/backend-2024/exerc4/demo/demo/src/main/java/com/example/demo/jogador/Jogador.java
@@ -0,0 +1,48 @@
+package com.example.demo.jogador;
+
+public class Jogador {
+    private String nome;
+    private String sobrenome;
+    private String posicao;
+    private int idade;
+    private String clube;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String paramNome) {
+        nome = paramNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String paramSobrenome){
+        sobrenome = paramSobrenome;
+    }
+
+    public String getPosicao(){
+        return posicao;
+    }
+
+    public void setPosicao(String paramPosicao){
+        posicao = paramPosicao;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int paramIdade){
+        idade = paramIdade;
+    }
+
+    public String getClube(){
+        return clube;
+    }
+
+    public void setClube(String paramClube){
+        clube = paramClube;
+    }
+}
